a058de88b15cb441ed8a2c683f50c548d0dff6fa
Marcelo Morais <marcelomorais.program@gmail.com>
2022-01-23 12:34:43 -0300

    teste

--- js/storage.js
+++ js/storage.js
@@ -41,8 +41,8 @@ async function salvarUrl(nomeImagem){
 }
 
 
-async function testes(){
-    
+async function renderizar(){
+
     let dados = await db.collection('imagensApp')
     let info = await dados.get()
     let html = ""
