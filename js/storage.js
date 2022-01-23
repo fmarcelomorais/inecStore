6ff8a225b2e00945e9046d5b15b24c087c73aa5c
Marcelo Morais <marcelomorais.program@gmail.com>
2022-01-23 20:11:39 -0300

    novas alteracoes

--- js/storage.js
+++ js/storage.js
@@ -69,16 +69,20 @@ async function renderizar(){
     
     info.forEach((app) =>{{            
             html +=
-            ` <div class="card text-white bg-primary mb-3" style="height: 10%; width: 90%;">
+            `
+            
+            <div class="card text-white bg-primary mb-3" style="height: 10%; width: 100%;">
             <div class="card-header">${app.data().nomeAluno}</div>
             <div class="card-body">
-            <img class="card-img-top mb-2" style="height: 30%; width: 30%; margin: 0 auto;" src="${app.data().urlImage}" alt="Imagem de capa do card">
+            <img class="card-img-top mb-2" style="height: 25%; width: 25%; margin: 0 auto;" src="${app.data().urlImage}" alt="Imagem de capa do card">
             <h5 class="card-title mt-3" >${app.data().nomeAplicativo}</h5>
             <p class="card-text">${app.data().descricao}</p>
             <hr>
             <a href="${app.data().urlArquivoApp}" class="btn btn-success mt-3">Baixar app</a>
             </div>
-            </div>`
+            </div>
+            
+            `
             }             
     })
 
