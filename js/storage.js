1b31f8f81043d73bd16461e5f63c7863fe9bef50
Marcelo Morais <marcelomorais.program@gmail.com>
2022-01-24 00:02:56 -0300

    novas alteracoes

--- js/storage.js
+++ js/storage.js
@@ -1,6 +1,7 @@
 const reference = firebase.storage()
 const db = firebase.firestore()
 
+
 let imagem = "";
 let nomeImagem = "";
 let urlImagem = "";
@@ -25,7 +26,13 @@ function carregarArquivo(e){
 
 // Salva a imagem e o arquivo no storage e pega a URL   
 
+
 async function salvarNoStorage(nomeImagem, imagem, nomeArquivo, arquivo){
+    const $nome = document.querySelector('.nome')
+    const $app = document.querySelector('.app')
+    const $descricao = document.querySelector('.descricao')
+
+
     await reference.ref('imagens/').child(nomeImagem).put(imagem)
     .then(() => swal("Salvo com Sucesso!", nomeImagem, "success"))
     .catch(erro => console.log(erro))
@@ -48,13 +55,17 @@ async function salvarNoStorage(nomeImagem, imagem, nomeArquivo, arquivo){
         //salvarUrlArquivo(nomeArquivo)
        })
 
-       await db.collection('aplicativos').doc(document.getElementById('app').value).set({
-        nomeAluno: document.getElementById('nome').value,
-        nomeAplicativo: document.getElementById('app').value,
-        descricao: document.getElementById('descricao').value,
+       await db.collection('aplicativos').doc($nome.value).set({
+        nomeAluno: $nome.value,
+        nomeAplicativo: $app.value,
+        descricao: $descricao.value,
         urlImage: urlImagem,
         urlArquivoApp: urlArquivo
     })
+
+    document.querySelector('.nome').value = ""
+    document.querySelector('.app').value = ""
+    document.querySelector('.descricao').value = ""
 
 
 }
@@ -72,11 +83,11 @@ async function renderizar(){
             `
             
             <div class="card text-white bg-primary mb-3" style="height: 10%; width: 100%;">
-            <div class="card-header">${app.data().nomeAluno}</div>
+            <div class="card-header">Aluno(a): <strong>${app.data().nomeAluno}</strong></div>
             <div class="card-body">
-            <img class="card-img-top mb-2" style="height: 25%; width: 25%; margin: 0 auto;" src="${app.data().urlImage}" alt="Imagem de capa do card">
-            <h5 class="card-title mt-3" >${app.data().nomeAplicativo}</h5>
-            <p class="card-text">${app.data().descricao}</p>
+            <img class="card-img-top mb-2"  src="${app.data().urlImage}" style="height: 20%; width: 20%; margin: 0 auto;" alt="Imagem de capa do card">
+            <p class="card-title mt-3" > Nome do Aplicativo: <strong>${app.data().nomeAplicativo}</strong></p>
+            <p class="card-text">Descrição: <strong>${app.data().descricao}</strong></p>
             <hr>
             <a href="${app.data().urlArquivoApp}" class="btn btn-success mt-3">Baixar app</a>
             </div>
